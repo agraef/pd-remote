c15e800b4941a8b4638538b3c203afd6b6451d15
Albert Graef <aggraef@gmail.com>
2023-02-20 20:34:36 +0100

    More changes to make package-lint happy.

--- pd-remote.el
+++ pd-remote.el
@@ -1,9 +1,10 @@
-;;; pd-remote.el --- Pd remote control stuff.
+;;; pd-remote.el --- Pd remote control helper
 
 ;; Copyright (c) 2023 Albert Graef
 
 ;; Author: Albert Graef <aggraef@gmail.com>
 ;; Keywords: multimedia, pure-data
+;; Version: 1.0.0
 ;; URL: https://github.com/agraef/pd-remote
 ;; License: MIT
 
@@ -23,34 +24,34 @@
 
 ;;; Code:
 
-(defun pd-send-start-process ()
+(defun pd-remote-start-process ()
   "Start a pdsend process to communicate with Pd via UDP port 4711."
   (interactive)
   (start-process "pdsend" nil "pdsend" "4711" "localhost" "udp")
   (set-process-query-on-exit-flag (get-process "pdsend") nil))
 
-(defun pd-send-stop-process ()
+(defun pd-remote-stop-process ()
   "Stops a previously started pdsend process."
   (interactive)
   (delete-process "pdsend"))
 
-(defun pd-send-message (message)
+(defun pd-remote-message (message)
   "Send the given MESSAGE to Pd.  Start the pdsend process if needed."
   (interactive "sMessage: ")
-  (unless (get-process "pdsend") (pd-send-start-process))
+  (unless (get-process "pdsend") (pd-remote-start-process))
   (process-send-string "pdsend" (concat message "\n")))
 
 ;; some convenient helpers
 
-(defun pd-dsp-on ()
+(defun pd-remote-dsp-on ()
   "Start dsp processing."
   (interactive)
-  (pd-send-message "pd dsp 1"))
+  (pd-remote-message "pd dsp 1"))
 
-(defun pd-dsp-off ()
+(defun pd-remote-dsp-off ()
   "Stop dsp processing."
   (interactive)
-  (pd-send-message "pd dsp 0"))
+  (pd-remote-message "pd dsp 0"))
 
 ;; Faust mode; this requires Juan Romero's Faust mode available at
 ;; https://github.com/rukano/emacs-faust-mode. NOTE: If you don't have this,
@@ -60,19 +61,18 @@
 
 ;; various convenient keybindings, factored out so that they can be used
 ;; in different keymaps
-(defun pd-define-keys (mode-map)
+(defun pd-remote-keys (mode-map)
   "Add common Pd keybindings to MODE-MAP."
-  (define-key mode-map "\C-c\C-m" #'pd-send-message)
+  (define-key mode-map "\C-c\C-m" #'pd-remote-message)
   (define-key mode-map "\C-c\C-s" #'(lambda () "Start" (interactive)
-				      (pd-send-message "play 1")))
+				      (pd-remote-message "play 1")))
   (define-key mode-map "\C-c\C-t" #'(lambda () "Stop" (interactive)
-				      (pd-send-message "play 0")))
-  (define-key mode-map "\C-c\C-g" #'(lambda () "Restart" (interactive)
-				      (pd-send-message "play 0")
-				      (pd-send-message "play 1")))
-  (define-key mode-map [(control ?\/)] #'pd-dsp-on)
-  (define-key mode-map [(control ?\.)] #'pd-dsp-off)
-  )
+				      (pd-remote-message "play 0")))
+  (define-key mode-map "\C-c\C-r" #'(lambda () "Restart" (interactive)
+				      (pd-remote-message "play 0")
+				      (pd-remote-message "play 1")))
+  (define-key mode-map [(control ?\/)] #'pd-remote-dsp-on)
+  (define-key mode-map [(control ?\.)] #'pd-remote-dsp-off))
 
 ;; Juan's Faust mode doesn't have a local keymap, add one.
 (defvar faust-mode-map nil)
@@ -81,9 +81,8 @@
   (setq faust-mode-map (make-sparse-keymap))
   ;; Some convenient keybindings for Faust mode.
   (define-key faust-mode-map "\C-c\C-k" #'(lambda () "Compile" (interactive)
-					    (pd-send-message "faustgen2~ compile")))
-  (pd-define-keys faust-mode-map)
-  ))
+					    (pd-remote-message "faustgen2~ compile")))
+  (pd-remote-keys faust-mode-map)))
 (add-hook 'faust-mode-hook #'(lambda () (use-local-map faust-mode-map)))
 
 ;; Lua mode: This requires lua-mode from MELPA.
@@ -95,13 +94,13 @@
 (define-key lua-mode-map "\C-c\C-d" #'lua-send-defun)
 (define-key lua-mode-map "\C-c\C-r" #'lua-send-region)
 ; Pd tie-in (see pd-lua tutorial)
-(pd-define-keys lua-mode-map)
+(pd-remote-keys lua-mode-map)
 (define-key lua-mode-map "\C-c\C-k" #'(lambda () "Reload" (interactive)
-					(pd-send-message "pdluax reload")))
+					(pd-remote-message "pdluax reload")))
 
 ;; add any convenient global keybindings here
-;(global-set-key [(control ?\/)] #'pd-dsp-on)
-;(global-set-key [(control ?\.)] #'pd-dsp-off)
+;(global-set-key [(control ?\/)] #'pd-remote-dsp-on)
+;(global-set-key [(control ?\.)] #'pd-remote-dsp-off)
 
 (provide 'pd-remote)
 
